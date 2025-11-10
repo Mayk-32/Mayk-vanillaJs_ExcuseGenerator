7142bd5c50019e94e4b144b742be4471ddbafd20
Aykut <m.aykutarslan1@gmail.com>
2025-11-10 23:53:14 +0000

    Excuse Generator done

--- src/app.js
+++ src/app.js
@@ -2,10 +2,23 @@ import "bootstrap";
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ['The cat', 'My cousin', 'The mailman', 'My bird', 'my younger brother'];
+  let action = ['puked', 'peed', 'threw away', 'broke' , 'hid'];
+  let what = ['my homework', 'my phone', 'my computer', 'the car', 'the weather'];
+  let when = ['during the class', 'when I was sleeping', 'while I was at work', 'during my lunch', 'while I was praying'];
+
+  const randomWho = who[Math.floor(Math.random() * who.length)];
+  const randomAction = action[Math.floor(Math.random() * action.length)];
+  const randomWhat = what[Math.floor(Math.random() * what.length)];
+  const randomWhen = when[Math.floor(Math.random() * when.length)];
+
+  const excuses = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`
+     
+  document.getElementById("Excuses").innerHTML = excuses;
+    
+  console.log("Excuses");  
+  return "let who", "let action", "let what", "let when";
+  
 };
+  
